fecaf628c9b3482ada18c297ebcf2ea983913375
Abhi <aanand@argusoft.com>
2023-10-11 15:15:00 +0530

    using ngSwitch

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -10,14 +10,36 @@ import { Component, OnInit } from '@angular/core';
   
   <!-- TWO-WAY BINDING
   for using ng model we have to import formsmodule in app.module.ts -->
-  <input [(ngModel)]="name" type="text"> 
-  {{name}}
+  <!-- <input [(ngModel)]="name" type="text"> 
+  {{name}} -->
+
+  <!-- DIRECTIVES -->
+  <!-- <h2 *ngIf="displayName; else elseBlock">
+    Codevolution
+  </h2>
+
+  <ng-template #elseBlock>
+    <h2>
+      Name is hidden
+    </h2> -->
+
+    <!-- USING ngSwitch -->
+    <div [ngSwitch]="color">
+      <div *ngSwitchCase="'red'">you picked red color</div>
+      <div *ngSwitchCase="'blue'">you picked blue color</div>
+      <div *ngSwitchCase="'green'">you picked green color</div>
+      <div *ngSwitchDefault>pick again</div>
+    </div>
+
+
 `,
   styles: []
 })
 export class TestComponent implements OnInit{
 
-  public name ="";
+  // public name ="";
+  // displayName=false;
+  public color="yellow";
 
   constructor(){
 
@@ -25,10 +47,10 @@ export class TestComponent implements OnInit{
   ngOnInit() {
       
   }
-  logMessage(value: any){
-    console.log(value);
+  // logMessage(value: any){
+  //   console.log(value);
     
 
-  }
+  // }
 
 }
